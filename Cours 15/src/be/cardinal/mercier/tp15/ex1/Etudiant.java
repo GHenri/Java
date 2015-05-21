a2ce6c7030e135abc0574012149266b2f40a2744
GHenri <geimerhenri@gmail.com>
2015-05-21 19:19:42 +0200

    version 1.0

--- Cours 15/src/be/cardinal/mercier/tp15/ex1/Etudiant.java
+++ Cours 15/src/be/cardinal/mercier/tp15/ex1/Etudiant.java
@@ -6,6 +6,23 @@ public class Etudiant {
 	private String nom;
 	private static int dernierNumAttribue = 0;
 	private int num;
-	private ArrayList<Integer> listePoints = new Arraylist<Integer>();
+	private ArrayList<Integer> listePoints = new ArrayList<Integer>();
 
+
+
+//public static Etudiant listeEtu = new Etudiant ();
+
+	public Etudiant(String nom){
+		this.nom = nom;
+		this.num = ++ dernierNumAttribue; //pré-incrémentation
+	}
+	public double moyenne(){
+		double somme = 0;
+		for (Integer points ; points < ) {
+			somme += points;
+		}
+		if (listePoints.size() == 0) return 0;
+		return somme / listePoints.size();
+	}
+	
 }
