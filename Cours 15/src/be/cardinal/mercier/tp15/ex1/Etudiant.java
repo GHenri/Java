fa4f00e64c53c10b04d28f7c1cf334e4ee2d2887
GHenri <geimerhenri@gmail.com>
2015-05-21 19:16:38 +0200

    Version 1.0 de l'ex1 du cours 15

---
+++ Cours 15/src/be/cardinal/mercier/tp15/ex1/Etudiant.java
@@ -0,0 +1,11 @@
+package be.cardinal.mercier.tp15.ex1;
+
+import java.util.ArrayList;
+
+public class Etudiant {
+	private String nom;
+	private static int dernierNumAttribue = 0;
+	private int num;
+	private ArrayList<Integer> listePoints = new Arraylist<Integer>();
+
+}
