cea2eb385deaa1e5805ff8b3f0ded3a4bb52e32b
GHenri <geimerhenri@gmail.com>
2015-05-21 19:38:56 +0200

    V1.2

--- Cours 15/src/be/cardinal/mercier/tp15/ex1/Etudiant.java
+++ Cours 15/src/be/cardinal/mercier/tp15/ex1/Etudiant.java
@@ -18,11 +18,46 @@ public class Etudiant {
 	}
 	public double moyenne(){
 		double somme = 0;
-		for (Integer points ; points < ) {
+		//for (Integer points ; points < listePoints.size() ) {
 			somme += points;
-		}
+		//}
 		if (listePoints.size() == 0) return 0;
 		return somme / listePoints.size();
 	}
+	
+	public String toString() {
+		StringBuffer s = new StringBuffer("Nom : " + nom + " num : "+num+"\nPoints : ");
+		for (Integer points : ) {
+		s.append("\t" + points);
+		}
+		s.append("\n" + "moyenne = " + this.moyenne()+"\n");
+		String s2 = new String(s);
+		return s2;
+	}
+	
+	public static void ajouteEtu(Etudiant newEtu){
+		_._(newEtu);
+	}
+	
+	public void ajoutePoint(int newPoint){
+		____________.______(newPoint);
+	}
+	
+	/**
+	* @return une chaine de caractères qui représentent tous les étudiants de la liste
+	*/
+	public static String toStringAll(){
+		
+		StringBuffer sb = new StringBuffer();
+		for (Etudiant etudiant : __________________) {
+			sb.append(etudiant.toString());
+		}
+		
+		String s = new String(sb);
+		return s;
+	}
+	
+	
+	
 	
 }
